d79e0e9aa5000227a02289c01815d9d1cbac3bf3
Paula Guillen <paulaguillen.iq@gmail.com>
2022-10-13 10:43:39 -0300

    primera parte de header. falta validacion de usuario y contraseña

---
+++ js/login.js
@@ -0,0 +1,30 @@
+import { validateContraseña, validateUsuario } from "./validacionLogin";
+
+
+//const formToLogin = document.getElementById("formUsuario");
+//const modalLogin = document.getElementById("modalUsuario");
+
+
+
+
+/*
+class Usuario{
+    constructor(usuarioLogin,contraseñaLogin){
+        this.usuarioLogin= usuarioLogin
+        this.contraseñaLogin= contraseñaLogin
+    }
+}
+
+const usuarioLogin = document.getElementById('usuarioLogin')
+const contraseñaLogin = document.getElementById('contraseñaLogin')
+
+usuarioLogin.addEventListener('blur',(e)=>{
+    if(validateUsuario(e.target.value, usuarioLogin)){
+      usuarioLogin = e.target.value
+    }
+})
+contraseñaLogin.addEventListener('blur',(e)=>{
+    if(validateContraseña(e.target.value, contraseñaLogin)){
+        contraseñaLogin = e.target.value
+    }
+})*/
---
+++ js/validacionLogin.js
@@ -0,0 +1,52 @@
+const usuarioLogin = document.getElementById('usuarioLogin')
+const contraseñaLogin = document.getElementById('contraseñaLogin')
+
+
+let usuarioLog = 'admin@gmail.com'
+let contraseña= 'rolling'
+
+
+/*
+export const validateUsuario = (valor, campo) =>{
+    if(valor.trim() === usuarioLog){
+        campo.classList = 'form-control is-valid'
+        return true
+    }
+    campo.classList= 'form-control is-invalid'
+    return false
+}
+
+export const validateContraseña =(valor, campo) =>{
+    if(valor.trim() === contraseña){
+        campo.classList = 'form-control is-valid'
+        return true
+    }
+    campo.classList= 'form-control is-invalid'
+    return false
+    
+}*/
+
+function validacion(){
+    if(usuarioLogin.value === usuarioLog){
+        alert ('ERROR. Usuario incorrecto.')
+        return false
+    }
+    const regex = /^\d+$/;
+    if (!regex.test(valor)) {
+      return false;
+
+    }
+    if(usuarioLogin.value<1){
+        return false
+    }
+    
+    if(contraseñaLogin.value<1){
+        return false
+    }
+
+    if(contraseñaLogin.value === contraseña){
+        alert ('ERROR. Contraseña incorrecta.')
+        return false
+    }
+    return true
+}
