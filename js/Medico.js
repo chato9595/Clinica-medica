ee810b566acd28c070fd0f885a474b3c8d722c9f
Marcos Almiron <chato9595@gmail.com>
2022-10-25 12:08:48 -0300

    preparando para merge final

--- js/Medico.js
+++
@@ -1,9 +0,0 @@
-export class Medico{
-    constructor(nombre,apellido,telefono,especialidad){
-       
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.telefono = telefono;
-        this.especialidad = especialidad;
-    }
-}
